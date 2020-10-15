cbd7a55994369e285fe8b227b932ff72fafdcfa6
Yeray Darias <ydarias@gmail.com>
2020-10-15 15:49:48 +0200

    Additional requirements at hello world

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+const spanish = "Spanish"
+const french = "French"
+const englishGreetingsPrefix = "Hello, "
+const spanishGreetingPrefix = "Hola, "
+const frenchGreetingPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return "Hello, " + name + "!"
+
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) string {
+	switch language {
+	case spanish:
+		return spanishGreetingPrefix
+	case french:
+		return frenchGreetingPrefix
+	default:
+		return englishGreetingsPrefix
+	}
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,7 +3,6 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-
 	assertCorrectMessage := func(t *testing.T, got string, expected string) {
 		t.Helper()
 		if got != expected {
@@ -12,14 +11,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("should say hello with the given name", func(t *testing.T) {
-		got := Hello("Eve")
+		got := Hello("Eve", "")
 		expected := "Hello, Eve!"
 		assertCorrectMessage(t, got, expected)
 	})
 
 	t.Run("should say hello world if name is not given", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		expected := "Hello, World!"
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("should return the message in Spanish", func(t *testing.T) {
+		got := Hello("Eve", "Spanish")
+		expected := "Hola, Eve!"
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("should return the message in French", func(t *testing.T) {
+		got := Hello("Eve", "French")
+		expected := "Bonjour, Eve!"
 		assertCorrectMessage(t, got, expected)
 	})
 }
