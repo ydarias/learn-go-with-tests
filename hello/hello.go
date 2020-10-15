77a70063456593fe99cffb247d527e014e857ae6
Yeray Darias <ydarias@gmail.com>
2020-10-15 15:32:14 +0200

    Refactored the hello world tests

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return "Hello, " + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello(""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Eve")
-	expected := "Hello, Eve!"
 
-	if got != expected {
-		t.Errorf("got %q, was expecting %q", got, expected)
+	assertCorrectMessage := func(t *testing.T, got string, expected string) {
+		t.Helper()
+		if got != expected {
+			t.Errorf("got %q, was expecting %q", got, expected)
+		}
 	}
+
+	t.Run("should say hello with the given name", func(t *testing.T) {
+		got := Hello("Eve")
+		expected := "Hello, Eve!"
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("should say hello world if name is not given", func(t *testing.T) {
+		got := Hello("")
+		expected := "Hello, World!"
+		assertCorrectMessage(t, got, expected)
+	})
 }
